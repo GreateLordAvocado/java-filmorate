a259af47667d1ccaf62b85d97758e2bbfda30d7c
GreateLordAvocado <sinner.halo@yandex.ru>
2025-08-29 02:46:52 +0700

    Исправляем косяки X2

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
-
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
